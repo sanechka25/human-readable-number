207f0099e137448ca0a20c12dc9955ac9f308015
Alexandra <65849986+sanechka25@users.noreply.github.com>
2021-03-12 11:39:49 +0300

    docs: upload file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+    const numWords = require('num-words')
+    let res = numWords(number)
+    let str = res.split(' ')
+    for (var i = str.length; i--;) {
+        if (str[i] === 'and') {
+            str.splice(i, 1);
+        }
+    }
+
+    return str.join(' ')
 }
+
