8a1a2a13a8b48a97b69c5dfb1f5194e8a7da93de
Alexandra <65849986+sanechka25@users.noreply.github.com>
2021-03-12 20:01:02 +0300

    docs: upload file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-module.exports = function toReadable (number) {
-    const numWords = require('num-words')
-    let res = numWords(number)
-    let str = res.split(' ')
-    for (var i = str.length; i--;) {
-        if (str[i] === 'and') {
-            str.splice(i, 1);
-        }
+module.exports = function toReadable (num) {
+    var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+              'seventeen', 'eighteen', 'nineteen'];
+    var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+              'ninety'];
+
+    var numStr = num.toString();
+
+    if (num === 0) {
+        return 'zero';
     }
 
-    return str.join(' ')
-}
+    if (num < 20) {
+        return ones[num];
+    }
 
+    if (numStr.length === 2) {
+        if (ones[numStr[1]] !== '')
+            return tens[numStr[0]] + ' ' + ones[numStr[1]];
+        else
+            return tens[numStr[0]];
+    }
+
+    if (numStr.length == 3) {
+        if (numStr[1] === '0' && numStr[2] === '0')
+            return ones[numStr[0]] + ' hundred';
+        else
+            return ones[numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]));
+  }
+}
